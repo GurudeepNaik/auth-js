6c11169dfea655b059352ce3eacd5627c50aa32f
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-01-28 12:48:32 +0530

    added loading to all pages

--- app/auth/loading.tsx
+++ app/auth/loading.tsx
@@ -3,7 +3,7 @@ import { BeatLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="flex justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <BeatLoader />
     </div>
   );
