0d07088ff4c92fb847cf97236ba4209aad2e2d8b
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-01-28 12:45:10 +0530

    added loading to all pages

--- app/(protected)/admin/loading.tsx
+++ app/(protected)/admin/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { BeatLoader } from "react-spinners";
 
 const Loading = () => {
-  return <div>...loading</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <BeatLoader />
+    </div>
+  );
 };
 
 export default Loading;
